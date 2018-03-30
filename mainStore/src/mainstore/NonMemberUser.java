534e2e98d201bed2abb590f5350c71f8655e5df5
Emmy365 <32851732+Emmy365@users.noreply.github.com>
2018-03-30 10:30:52 -0500

    Add files via upload

---
+++ mainStore/src/mainstore/NonMemberUser.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uberstock;
+
+/**
+ *
+ * @author arnol
+ */
+public class  NonMemberUser extends User
+{
+    private boolean upgrade = false;
+    
+    public void setUpgrade(boolean x)
+    {
+        this.upgrade = x;
+    }
+    
+    public void Upgrade(char choice)
+    {
+        if(choice =='1')
+        {
+            this.setUpgrade(true);
+        }
+    }
+}
---
+++ mainStore/src/mainstore/User.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uberstock;
+
+/**
+ *
+ * @author arnol
+ */
+public abstract class User 
+{
+
+    private String FirstName;
+    private String LastName;
+    private String MiddleIntial;
+    private String EmailAddress;
+    private String Password;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
