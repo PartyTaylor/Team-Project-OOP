99e1484509be1d5fc2cf8cb5b4db6da5c46ceaae
PartyTaylor <33302487+PartyTaylor@users.noreply.github.com>
2018-03-29 23:00:47 -0500

    Update

---
+++ mainStore/src/mainstore/Category.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mainstore;
+
+/**
+ *
+ * @author Taylor Jones
+ * @author Emily Rodriguez 
+ */
+public abstract class Category {
+    
+    public abstract void showAvailableItems();
+    
+    public abstract void buyItem();
+    
+    public abstract void cancelItem();
+    
+    
+    
+}
